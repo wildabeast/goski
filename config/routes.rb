8bd58ac2122dadb2cf762f8a50ffa2e5d17f3dbc
Ryan Willoughby <ryan.willoughby@kinship.co>
2022-12-23 13:26:33 -0800

    create front-end scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   
+  # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
 end
